fd7709abd50f8841d266ea9eb7e57751afa1dd0c
Nathan Glover <nathan@glovers.id.au>
2017-03-04 23:36:52 +0800

    changed cat for smalls

--- index.js
+++ index.js
@@ -12,73 +12,74 @@ require("colors");
 const METRIC = process.argv.includes("--metric");
 const GRAMS_PER_OZ = 28.3495;
 const SFSPCA_BASE = "https://www.sfspca.org"
-const ADOPTION_PAGE = `${SFSPCA_BASE}/adoptions/cats`;
+const ADOPTION_PAGE = `${SFSPCA_BASE}/adoptions/smalls`;
 
-const fetchCatsHelper = Promise.method((pageNumber, catsSoFar) => {
+const fetchsmallsHelper = Promise.method((pageNumber, smallsSoFar) => {
   const url = pageNumber === 0 ? ADOPTION_PAGE : `${ADOPTION_PAGE}?page=${pageNumber}`
   return request.get(url)
     .then((adoptionsPage) => {
-      const cats = cheerio(adoptionsPage)
+      const smalls = cheerio(adoptionsPage)
         .find("a")
         .filter((i, tag) => tag.attribs.href && tag.attribs.href.match(/adoptions\/pet-details\/\d+/))
         .map((i, tag) => `${SFSPCA_BASE}${tag.attribs.href}`)
         .toArray();
-      if (!cats || cats.length === 0) {
-        return catsSoFar;
+      if (!smalls || smalls.length === 0) {
+        return smallsSoFar;
       } else {
-        return fetchCatsHelper(pageNumber + 1, catsSoFar.concat(cats));
+        return fetchsmallsHelper(pageNumber + 1, smallsSoFar.concat(smalls));
       }
     })
     .catch((err) => {
-      console.log("Error fetching cats:", err);
-      return catsSoFar;
+      console.log("Error fetching smalls:", err);
+      return smallsSoFar;
     });
 });
 
-console.log("Accessing San Francisco SPCA (Cat Department)...");
-fetchCatsHelper(0, [])
-  .then(_.uniq) // NO DOUBLE CATS
-  .tap((cats) => console.log(`Cat information system accessed. ${cats.length} cats found. Beginning weighing process...`))
+console.log("Accessing San Francisco SPCA (Smalls Department)...");
+fetchsmallsHelper(0, [])
+  .then(_.uniq) // NO DOUBLE smalls
+  .tap((smalls) => console.log(`Smalls information system accessed. ${smalls.length} smalls found. Beginning weighing process...`))
   .map((url) => {
     return request.get(url)
-      // SPCA sometimes returns 403s for some cats, ignore this.
+      // SPCA sometimes returns 403s for some smalls, ignore this.
       .catch((err) => err)
       .then((catPage) => {
         const $ = cheerio.load(catPage);
         const name = $(".field-name-title h1").text();
         const weightText = $(".field-name-field-animal-weight .field-item").text();
+        const type = $(".field-name-field-animal-type .field-item").text().trim();
         const lbs = Number(/(\d+)lbs\./.exec(weightText)[1]);
         const oz = /(\d+)oz\./.test(weightText) ? Number(/(\d+)oz\./.exec(weightText)[1]) : 0;
         const weight = 16 * lbs + oz;
         const isFemale = $(".field-name-field-gender .field-item").text().trim() === "Female";
 
-        console.log("Weighing cat: %s", name.green);
-        return {name, lbs, oz, weight, isFemale, url}
+        console.log("Weighing a %s named %s", type.blue, name.green);
+        return {name, type, lbs, oz, weight, isFemale, url}
       })
-      // Null for cats that cannot be parsed.
+      // Null for smalls that cannot be parsed.
       .catch(() => {});
   })
-  // Filter out unparsable cats.
+  // Filter out unparsable smalls.
   .then(_.compact)
-  .then((cats) => {
-    if (cats.length === 0) {
-      console.log("No cats found. It is a sad day.".red.bold);
+  .then((smalls) => {
+    if (smalls.length === 0) {
+      console.log("No smalls found. It is a sad day.".red.bold);
       return;
     }
 
-    const highestWeight = _(cats).map("weight").max();
-    const fattestCats = _.filter(cats, {weight: highestWeight});
-    const names = _.map(fattestCats, "name");
-    const tie = fattestCats.length > 1;
+    const highestWeight = _(smalls).map("weight").max();
+    const fattestsmalls = _.filter(smalls, {weight: highestWeight});
+    const names = _.map(fattestsmalls, "name");
+    const tie = fattestsmalls.length > 1;
 
-    const introText = (tie ? "The fattest cats are" : "The fattest cat is").yellow.bold;
+    const introText = (tie ? "The fattest smalls are" : "The fattest small is").yellow.bold;
     const nameText = (tie ? `${names.slice(0, -1).join(", ")} and ${_.last(names)}` : names[0]).green.underline.bold;
-    const descriptionText = (tie ? "They each weigh" : (fattestCats[0].isFemale ? "She weighs" : "He weighs")).yellow.bold;
+    const descriptionText = (tie ? "They each weigh" : (fattestsmalls[0].isFemale ? "She weighs" : "He weighs")).yellow.bold;
     const weightText = METRIC ?
       (`${Math.round(GRAMS_PER_OZ * highestWeight)} grams`).yellow.bold :
-      (`${fattestCats[0].lbs} lbs and ${fattestCats[0].oz} oz.`).yellow.bold;
-    const openText = (tie ? "Opening cat profiles..." : "Opening cat profile...").yellow.bold;
+      (`${fattestsmalls[0].lbs} lbs and ${fattestsmalls[0].oz} oz.`).yellow.bold;
+    const openText = (tie ? "Opening smalls profiles..." : "Opening small profile...").yellow.bold;
 
     console.log(`${introText} ${nameText}. ${descriptionText} ${weightText}. ${openText}`);
-    setTimeout(() => _(fattestCats).map("url").each(opener), 3000);
+    setTimeout(() => _(fattestsmalls).map("url").each(opener), 3000);
   });
